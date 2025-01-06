11c3c3faa67041d9e59025a91bc1d0263b59d47f
Windisley <windisleyl@gmail.com>
2025-01-06 18:45:29 -0300

    Logica Do Carrossel Pronta, Feita Com Javascript

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const translateCarrossel = document.querySelector(".translate-carrossel")
 const arrowLeft = document.querySelector(".bi-arrow-left")
 const arrowRight = document.querySelector(".bi-arrow-right")
+
+
+
+
+let count = 0;
+arrowRight.addEventListener("click", ()=>{
+    
+    if(count <= 4 -1){
+            count++
+            translateCarrossel.style.transform = `translateX(-${count * 100}%)`;
+
+        }
+        console.log(translateCarrossel)
+        console.log(count)
+
+
+})
+
+
+arrowLeft.addEventListener("click", ()=>{
+     
+    if(count <= 5 && count > 0){
+        count--
+        translateCarrossel.style.transform = `translateX(-${count * 100}%)`;
+
+    }
+
+})
