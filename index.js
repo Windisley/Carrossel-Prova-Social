3009cc829f594f0a5185411901e948d9ac532baf
Windisley <windisleyl@gmail.com>
2025-01-06 17:14:54 -0300

    Criando Variaveis Javascript

---
+++ index.js
@@ -0,0 +1,3 @@
+const translateCarrossel = document.querySelector(".translate-carrossel")
+const arrowLeft = document.querySelector(".bi-arrow-left")
+const arrowRight = document.querySelector(".bi-arrow-right")
